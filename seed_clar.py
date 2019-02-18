2bf6090c17fdd70d3ba58994f9124cfd1102918a
Hackbright Student <no-reply@hackbrightacademy.com>
2019-02-18 15:25:38 -0800

    Cannot parse url yet

--- seed_clar.py
+++ seed_clar.py
@@ -5,13 +5,62 @@ FROM CLARIFAI API - gets images
 
 # API Requests / Get
 from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
+
+import pandas as pd
+
+from model import User, Photo, Bee, connect_to_db, db
+from server import app
+
 clarifai_app = ClarifaiApp(api_key="58dc8755e39d4043a98554b44bbcaf56")
 # model = app.public_models.general_model
 # response = model.predict_by_url('url!!')
 
-# Local scripts
-from model import User, Photo, Bee, connect_to_db, db
-from server import app
+def images_up_with_concepts(csv_filename):
+    """
+    Load images to Clarifai model, using custom tags from csv file.
+    """
+    print("csv file")
+
+    # df is a pandas DataFrame
+    df = pd.read_csv(csv_filename, # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
+                    index_col=False, # from docs: tells reader to ignore headers
+                    parse_dates={'datetime':[1, 2]}, # from docs: This will parse columns 1, 2 as date and call result 'datetime'
+                    dtype={'subspecies':'category', 'health':'category'} # Data type for data, column respectively.
+    )
+
+    image_list = []
+
+    # for i in range(len(df)):
+    for i in range(30): # FOR NOW
+
+        # bee_id = i # Integer
+
+        health_ = str(df.loc[i][5])
+        # datetime = df.loc[i][0]
+        local_filename = str(df.loc[i][1])
+        # location = str(df.loc[i][2])
+        # zip_code = str(df.loc[i][3])
+        # subspecies = str(df.loc[i][4])
+        # pollen_carrying = str(df.loc[i][6])
+        # caste = str(df.loc[i][7])
+
+
+        # Edit health (a string) to make it better for queries (a binary value)
+        health = 'y' if health_ == 'healthy' else 'n'
+        
+
+        if (health == 'y'):
+            img = ClImage(local_filename,  concepts=['health'])
+        else:
+            img = ClImage(local_filename, not_concepts=['health'])
+
+        image_list.append(img)
+
+    clarifai_app.inputs.bulk_create_images(image_list)
+
+
+
 
 
 def load_bees_from_clarifai():
@@ -58,8 +107,6 @@ def load_bees_from_clarifai():
 
 
 
-
-
     # Commit all bees to the database
     db.session.commit()
 
@@ -68,4 +115,7 @@ if __name__ == '__main__':
     connect_to_db(app)
     db.create_all()
 
-    load_bees_from_clarifai()
+    seed_filename = "bee_data.csv" 
+    images_up_with_concepts(seed_filename)
+
+    # load_bees_from_clarifai()
